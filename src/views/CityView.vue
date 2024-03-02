4d83c7f271afe9f539a222546ad33e63d1c5a33c
chenye <3298365564@qq.com>
2024-03-03 03:55:56 +0800

    MainView update

--- src/views/CityView.vue
+++ src/views/CityView.vue
@@ -1,12 +1,14 @@
 <script setup>
 import BigTitle from "@/components/BigTitle.vue";
 import FootLoop from '@/components/FootLoop.vue';
+import ChooseTable from "@/components/SecondView/ChooseTable.vue";
 </script>
 
 <template>
     <div class="CityMain">
         <BigTitle id="BigTitle"></BigTitle>
         <FootLoop id="FootLoop"></FootLoop>
+        <ChooseTable id="ChooseTable"></ChooseTable>
     </div>
 </template>
 
@@ -14,6 +16,11 @@ import FootLoop from '@/components/FootLoop.vue';
 .CityMain{
     #BigTitle{
         width: 100vw;
+    }
+
+    #ChooseTable{
+        width: 2vw;
+        height: 1vh;
     }
 
     #FootLoop {
