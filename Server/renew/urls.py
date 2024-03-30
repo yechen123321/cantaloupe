67c4fb5175f79e0384c7d11203ea463a8b4f20ff
RuoLi <1069121098@qq.com>
2024-03-30 21:52:41 +0800

    EnergyReserveModel

--- Server/renew/urls.py
+++ Server/renew/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('get_region_energy_production/<int:id>/', get_region_energy_production, name='get_region_energy_production'),
     #  地区资源设施使用情况
     path('get_regional_resource_facilities/<int:id>/', get_regional_resource_facilities, name='get_regional_resource_facilities'),
+    #  地区再生能源储量
+    path('get_energy_reserve/<int:id>/', get_energy_reserve, name='get_energy_reserve'),
+
 ]
