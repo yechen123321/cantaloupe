d6b60d4058c93dfe8de7a36c4e58d505b479eb76
RuoLii <146577867+RuoLii@users.noreply.github.com>
2024-04-23 03:04:16 +0800

    Delete Server directory

--- Server/PowerGenerationBase/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class PowergenerationbaseConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'PowerGenerationBase'
-    verbose_name = '发电基地'
--- Server/SuYuanApplication/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class HomeConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "SuYuanApplication"
-    verbose_name = '全国能源概览'
-    
